0e4450b2866dd608e918df01341e25e8609ed3ed
Kollan House <kollanh@gmail.com>
2019-12-11 20:47:11 -0500

    added in GA code.

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
